f349a6ed3280a6424a7f5e167aa1cd056eb8d433
abbeynox <git@ywk.ch>
2024-04-18 11:39:41 +0200

    beautiful formatting for operator category

--- sbb.sh
+++ sbb.sh
@@ -4,15 +4,15 @@ SBB_HTTP_URL="http://fahrplan.sbb.ch/bin/query.exe/dn?jumpToDetails=yes"
 SBB_API_BASE='http://transport.opendata.ch/v1/connections'
 
 function usage {
-    echo "Usage ./sbb.sh OPTIONS FROM TO"
+    echo "Usage sbb OPTIONS FROM TO"
     echo "  OPTIONS:"
     echo "      -t  Time to search for connections"
     echo "      -d  Date to search for connections"
     echo "      -v  Spevifies a via Station"
     echo "      -a  Uses the Date and Time for Arrival"
-    echo "      -i  Use it and be happy"
     exit
 }
+
 
 function append_if_not_empty {
     if [ -z "$2" ] || [ "$2" = "null" ] ; then
@@ -20,6 +20,47 @@ function append_if_not_empty {
     else
         echo "$1$3"
     fi
+}
+
+format_product() {
+    local category=$1
+    local number=$2
+
+    case $category in
+        "T")
+            emoji="🚊T"
+            ;;
+        "B")
+            emoji="🚌B"
+            background_color="\e[43m"
+            ;;
+        "S")
+            background_color="\e[44m"
+            ;;
+        "IR" | "IC" | "ICE" | "TGV" | "EC" | "NJ" | "IRE")
+            background_color="\e[41m"
+            font_style="\e[3;1m"
+            ;;
+        "RE")
+            background_color="\e[47m" 
+            font_style="\e[31m"
+            ;;
+        *)
+            #background_color="\e[47m"  
+            ;;
+    esac
+
+    if [ -n "$emoji" ]; then
+        formatted_product="${emoji}${number}"
+    else
+        formatted_product="${category}${number}"
+    fi
+
+    if [ "$category" = "ICE" ] || [ "$category" = "EC" ] || [ "$category" = "TGV" ] || [ "$category" = "NJ" ]; then
+        formatted_product="${category}"
+    fi
+
+    echo -e "\e[97m${background_color}${font_style} ${formatted_product} \e[0m"
 }
 
 function request_connection {
@@ -33,137 +74,100 @@ function request_connection {
     
     local i_connection=0
     echo $resultJson | jq -r '.connections[].duration' | while read connection; do
-	print_connection "$resultJson" ".connections[$i_connection]"
-	(( i_connection++ ))
+        print_connection "$resultJson" ".connections[$i_connection]"
+        (( i_connection++ ))
     done
 }
-
 function print_connection {
+    local divider_top="\e[1m╭───────────────────────────────.★..──────╮\e[0m"
+    local divider_bottom="\e[1m╰──────..★.───────────────────────────────╯\e[0m \n"
+
+    echo -e $divider_top
+
     print_connection_header "$1" "$2"
 
     local i_section=0
     echo $1 | jq -r "$2.sections[].departure.station.name" | while read section; do
-	print_section "$1" "$2.sections[$i_section]" $i_section
-	(( i_section++ ))
+        print_section "$1" "$2.sections[$i_section]" $i_section
+        (( i_section++ ))
     done
+
+    echo -e $divider_bottom
 }
 
-function print_connection_header {
-    echo ""
-    
+function print_connection_header {    
     station_from=$(echo $1 | jq -r "$2.from.station.name")
     station_to=$(echo $1 | jq -r "$2.to.station.name")
 
-    echo "$station_from - $station_to"
-    
+    echo -e "\e[1m$station_from → $station_to\e[0m" 
+
     if [[ "$os" = Linux ]]; then
         departure_time=$(date -d @$(echo $1 | jq -r "$2.from.departureTimestamp") +"%H:%M")
         arrival_time=$(date -d @$(echo $1 | jq -r "$2.to.arrivalTimestamp") +"%H:%M")
-	duration=$(echo $(echo $1 | jq -r "$2.duration") | sed -r 's/^[0-9]+d//gi')
+        duration=$(echo $(echo $1 | jq -r "$2.duration") | sed -r 's/^[0-9]+d//gi')
     elif [[ "$os" = "macOS" ]]; then
         departure_time=$(date -j -f "%s" $(echo $1 | jq -r "$2.from.departureTimestamp") +"%H:%M")
         arrival_time=$(date -j -f "%s" $(echo $1 | jq -r "$2.to.arrivalTimestamp") +"%H:%M")
-	duration=$(echo $(echo $1 | jq -r "$2.duration") | sed -E 's/^[0-9]+d//g')
+        duration=$(echo $(echo $1 | jq -r "$2.duration") | sed -E 's/^[0-9]+d//g')
     else
-	echo "Unkown OS"
-	exit 1
+        echo "Unkown OS"
+        exit 1
     fi
     transfers=$(echo $1 | jq -r "$2.transfers")
 
-    echo "Abfahrt: $departure_time"
-    echo "Ankunft: $arrival_time"
+    echo -e "Abfahrt: \x1b[37;42m$departure_time\e[0m"
+    echo -e "Ankunft: \x1b[37;44m$arrival_time\e[0m" 
     echo "Dauer: $duration, Umsteigen: $transfers"
 }
 
 function print_section {
     station=$(printf "%-15s" "$(echo $1 | jq -r "$2.departure.station.name")")
     if [[ "$os" = Linux ]]; then
-	stime=$(date -d @$(echo $1 | jq -r "$2.departure.departureTimestamp") +"%H:%M")
+        stime=$(date -d @$(echo $1 | jq -r "$2.departure.departureTimestamp") +"%H:%M")
     elif [[ "$os" = "macOS" ]]; then
-	stime=$(date -j -f "%s" $(echo $1 | jq -r "$2.departure.departureTimestamp") +"%H:%M")
+        stime=$(date -j -f "%s" $(echo $1 | jq -r "$2.departure.departureTimestamp") +"%H:%M")
     else
-	echo "Unkown OS"
-	exit 1
+        echo "Unkown OS"
+        exit 1
     fi
     platform=$(echo $1 | jq -r "$2.departure.platform")
-    product=$(echo $1 | jq -r "$2.journey.name")
+    productCategory=$(echo $1 | jq -r "$2.journey.category")
+    productNumber=$(echo $1 | jq -r "$2.journey.number")
+    product=$(format_product "$productCategory" "$productNumber")
     walk_object=$(echo $1 | jq -r "$2.walk")
 
     if [ $i_section -ne 0 ] && [ "$walk_object" = "null" ] ; then
-	prev_section=".connections[$i_connection].sections[$(($i_section - 1))]"
-	prev_walk_object=$(echo $1 | jq -r "$prev_section.walk")
-	if [ "$prev_walk_object" = "null" ] ; then
-	   echo "|  - [Umsteigen]"
-	fi
+        prev_section=".connections[$i_connection].sections[$(($i_section - 1))]"
+        prev_walk_object=$(echo $1 | jq -r "$prev_section.walk")
+        if [ "$prev_walk_object" = "null" ] ; then
+           echo "|  - [Umsteigen]"
+        fi
     fi
 
     if [ "$walk_object" != "null" ] ; then
-	echo "|  - [Fussweg]"
+        echo "|  - [Fussweg]"
     else
-	departure_string="+- $station ab $stime"
-	departure_string=$(append_if_not_empty "$departure_string" "$platform" ": Gleis $platform")
-	departure_string=$(append_if_not_empty "$departure_string" "$product" " [$product]")
-	echo "$departure_string"
+        departure_string="$product $station ab $stime"
+        departure_string=$(append_if_not_empty "$departure_string" "$platform" ": Gleis $platform")
+        departure_string=$(append_if_not_empty "$departure_string" "$product")
+        echo "$departure_string"
 
         station=$(printf "%-15s" "$(echo $1 | jq -r "$2.arrival.station.name")")
         if [[ "$os" = Linux ]]; then
-	    stime=$(date -d @$(echo $1 | jq -r "$2.arrival.arrivalTimestamp") +"%H:%M")
+            stime=$(date -d @$(echo $1 | jq -r "$2.arrival.arrivalTimestamp") +"%H:%M")
         elif [[ "$os" = "macOS" ]]; then
-	    stime=$(date -j -f "%s" $(echo $1 | jq -r "$2.arrival.arrivalTimestamp") +"%H:%M")
+            stime=$(date -j -f "%s" $(echo $1 | jq -r "$2.arrival.arrivalTimestamp") +"%H:%M")
         else
-	    echo "Unkown OS"
-	    exit 1
-	fi
-	
-	platform=$(echo $1 | jq -r "$2.arrival.platform")
+            echo "Unkown OS"
+            exit 1
+        fi
 
-	arrival_string="+- $station an $stime"
-	arrival_string=$(append_if_not_empty "$arrival_string" "$platform" ": Gleis $platform")
+        platform=$(echo $1 | jq -r "$2.arrival.platform")
+
+        arrival_string="+- $station an $stime"
+        arrival_string=$(append_if_not_empty "$arrival_string" "$platform" ": Gleis $platform")
         echo "$arrival_string"
     fi
-}
-
-function print_ascii_art {
-    echo " ____ ____ ____ _________ ____ ____ ____ ____ ____ "
-    echo "||C |||L |||I |||       |||R |||U |||L |||E |||Z ||"
-    echo "||__|||__|||__|||_______|||__|||__|||__|||__|||__||"
-    echo '|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|'
-    echo ""
-    echo "Created by absturztaube <me@absturztau.be>"
-    echo "Contributors:"
-    echo " - nohillside"
-    echo ""
-    echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░▒▓██████▓▓▓███▓▒░░░░░░░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░▒████████████████████▓░░░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░▒████████████████████████▓░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▓▓██████████████████████████▒░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░█████████████████████████████▒░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░▓█████████████████████████████▓░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░█████████████████████████████████▓░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░▒██████████████████▓█████████████████▓░░░░░░░░"
-    echo "░░░░░░░░░░░░▒▓███▒███████████▓▒▒▒▓██████████████▓▒░░░░░░░░░"
-    echo "░░░░░░░░░░░░░▓██▒▒▓█████▓██▓▒▒▒▒▒▓█████████████▒░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░▒▓▒▒▒▒▒▒█▓▒▒▒▒▒▒▒▒▒▒▒████████████▓░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓████████████▒░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓████████████▒░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▓█████▒▒▒▒▒▓███████▒▒▒███████████▒░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▓▓▒░░▒▓▒▒▒▒▓▒░░░░▒▓▒▒▒███████████▒░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▒▒░░▒▓▓▒▒▒▓░░░░░▓░▒▒▒▒███████████▒░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░▒▓▒▓▓▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒█▒▓███████▒░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░▒▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▒█████▓░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▒▓▒▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓█████▒░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▒▒▒▒▒▓▓▓▒▒▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒████▒░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▒▒▒▒▒▒▒▒▓████▒░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░▒▓▓▒▒▒▒▒▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▓███▓░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░░▓▓▒▒▒▒▒▓▓▒▒▒▒▒▒▒▒▒▒████░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░░░░██████▓▒▒▒▒▒▒▒▒▒▒▓▒░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░░░░▒████▓▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░"
-    echo "░░░░░░░░░░░░░░░░░░░░░░░░▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░"
-    echo ""
-    exit
 }
 
 os=Linux
@@ -178,14 +182,14 @@ while getopts ":t:d:v:aiV" o; do
             ;;
         d)
             datestamp_raw=${OPTARG}
-	    if [[ "$os" = Linux ]]; then
-		datestamp=$(date -d "$(echo $datestamp_raw | sed -r 's/([0-9]+)\.([0-9]+)\.([0-9]+)/\3-\2-\1/')" +"%d.%m.%Y")
-	    elif [[ "$os" = "macOS" ]]; then
-		datestamp=$(date -j -f "%Y-%m-%d" "$(echo $datestamp_raw | sed -E 's/([0-9]+)\.([0-9]+)\.([0-9]+)/\3-\2-\1/')" +"%d.%m.%Y")
-	    else
-		echo "Unkown OS"
-		exit 1
-	    fi
+            if [[ "$os" = Linux ]]; then
+                datestamp=$(date -d "$(echo $datestamp_raw | sed -r 's/([0-9]+)\.([0-9]+)\.([0-9]+)/\3-\2-\1/')" +"%d.%m.%Y")
+            elif [[ "$os" = "macOS" ]]; then
+                datestamp=$(date -j -f "%Y-%m-%d" "$(echo $datestamp_raw | sed -E 's/([0-9]+)\.([0-9]+)\.([0-9]+)/\3-\2-\1/')" +"%d.%m.%Y")
+            else
+                echo "Unkown OS"
+                exit 1
+            fi
             ;;
         v)
             via=${OPTARG}
@@ -193,9 +197,6 @@ while getopts ":t:d:v:aiV" o; do
         a)
             arrival=1
             ;;
-	i)
-	    print_ascii_art
-	    ;;
         *)
             usage
             ;;
